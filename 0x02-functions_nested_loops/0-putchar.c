53372ebb31bb12fa2f10dd5a9e0b7fdd116f271d
Phyllis Anim <nanaanim163@gmail.com>
2023-02-21 04:46:50 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+	/**
+	 * main -A program that prints _putchar that follows a new line
+	 * is written by mbah
+	 * Return: 0 (SUccess)
+	 */
+
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
